694b52c8440cbaa33acbfd1839e524fd465f8ac8
R.sewda <sewdaraghav@gmail.com>
2025-01-20 02:02:00 +0530

    few changes

--- src/app.js
+++ src/app.js
@@ -29,10 +29,8 @@ app.use("/", userRouter);
 connectDB()
   .then(() => {
     console.log("Database connection established..");
-    app.listen(process.env.PORT, () => {
-      console.log(
-        `Server is successfully listening on port ${process.env.PORT}...`
-      );
+    app.listen(7777, () => {
+      console.log(`Server is successfully listening on port 7777...`);
     });
   })
   .catch((err) => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+  await mongoose.connect(
+    "mongodb+srv://sewdaraghav:fH2NQsTsrgeqqczi@nodezen.tlajk.mongodb.net/devTinder"
+  );
 };
 
 module.exports = connectDB;
