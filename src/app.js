99ce09156726b4c462416f38ba55d383a7559a4b
R.sewda <sewdaraghav@gmail.com>
2025-02-01 23:46:35 +0530

    added .env file

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use("/", userRouter);
 connectDB()
   .then(() => {
     console.log("Database connection established..");
-    app.listen(7777, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`Server is successfully listening on port 7777...`);
     });
   })
